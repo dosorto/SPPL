992dba61a551c6661bff657057c9773b897fe50a
Cristian Betancourth <betancourthcristian698@gmail.com>
2025-07-30 20:32:19 -0600

    pequnio cambio

--- database/migrations/2025_06_24_032503_create_caja_aperturas_table.php
+++ database/migrations/2025_06_24_032503_create_caja_aperturas_table.php
@@ -10,26 +10,14 @@ return new class extends Migration
     {
         Schema::create('caja_aperturas', function (Blueprint $table) {
             $table->id();
-            
-            // Relaciona con el usuario que abre la caja.
             $table->foreignId('user_id')->constrained();
-
-            // Monto inicial (L. 2000.00 por defecto).
             $table->decimal('monto_inicial', 10, 2);
-
-            // Se llenará al momento de cerrar la caja. Nulable mientras esté abierta.
             $table->decimal('monto_final_calculado', 10, 2)->nullable();
-            
-            // Timestamp de apertura. Se establece al crear el registro.
             $table->timestamp('fecha_apertura');
-
-            // Timestamp de cierre. Nulable mientras la caja esté abierta.
             $table->timestamp('fecha_cierre')->nullable();
-
-            // Estado para saber si la caja está 'ABIERTA' o 'CERRADA'.
             $table->string('estado', 20)->default('ABIERTA');
             
-            $table->timestamps(); // Laravel gestiona created_at y updated_at
+            $table->timestamps(); 
         });
         
     }
